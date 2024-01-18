17aad16da774b5a9020a9ea203e4269fb219bd1c
Okwoli-Godwin <okwolig60@gmail.com>
2024-01-18 15:22:59 +0100

    agent login page started

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,11 +1,16 @@
 import { useRoutes } from "react-router-dom";
 import { Dashboardlayout } from "../components";
 import Dashboardhome from "../pages/Dashboard/Dashboardhome";
+import Login from "../pages/Auth/Login";
 
 const Routes = () => {
     let element = useRoutes([
         {
-           path: "/",
+            path: "/",
+            element: <Login />
+        },
+        {
+           path: "/agent",
            element: <Dashboardlayout />,
            children: [
                {
