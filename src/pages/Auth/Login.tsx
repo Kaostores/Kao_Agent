4a36f21cc598e3602f54899ed4f78b2e229760ed
Okwoli-Godwin <okwolig60@gmail.com>
2024-01-18 16:36:59 +0100

    update

--- src/pages/Auth/Login.tsx
+++ src/pages/Auth/Login.tsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import img from "../../assets/Design stats-amico.png"
+import styled from "styled-components"
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <Container>
+        <Left>
+            vvvvjvjvj
+        </Left>
+    </Container>
   )
 }
 
 export default Login
+const Left = styled.div`
+    width: 50%;
+    height: 100%;
+    background-color: red;
+`
+
+const Container = styled.div`
+    width: 100%;
+    height: 100%;
+    display: flex;
+`
