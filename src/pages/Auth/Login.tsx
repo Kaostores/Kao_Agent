6853201e3c2050872cb91f97ad0fec2e8f8c697a
Okwoli-Godwin <okwolig60@gmail.com>
2024-01-19 06:50:48 +0100

    update

--- src/pages/Auth/Login.tsx
+++ src/pages/Auth/Login.tsx
@@ -1,37 +1,49 @@
 import React from 'react'
 import img from "../../assets/Design stats-amico.png"
+import img2 from "../../assets/design.png"
 import styled from "styled-components"
 
 const Login = () => {
   return (
     <Container>
         <Left>
-            <img src={img} alt="" />
-            <Card></Card>
+            <Imageholder>
+                {/* <img src={img2} alt="img1" /> */}
+            </Imageholder>
+            <Card>
+                <img src={img} alt="" />
+            </Card>
         </Left>
     </Container>
   )
 }
 
 export default Login
+const Imageholder = styled.div`
+    width: 100%;
+    height: 100%;
+    background-image: url(${img2});
+    background-position: center;
+    background-size: cover;
+    background-repeat: no-repeat;
+`
 const Card = styled.div`
     width: 100%;
     height: 100%;
     position: absolute;
     top: 0;
     background-color: #0031ade4;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    img{
+        height: 380px;
+    }
 `
 const Left = styled.div`
     width: 50%;
     height: 100%;
-    /* background-color: red; */
     position: relative;
-    img{
-        width: 100%;
-        height: 100%;
-        object-fit: cover;
-    }
-    
 `
 
 const Container = styled.div`
