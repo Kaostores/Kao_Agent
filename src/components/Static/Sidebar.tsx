e23b7b4d3b343c9aa3b51b2361c10c3a25b86b14
Okwoli-Godwin <okwolig60@gmail.com>
2024-01-19 14:04:38 +0100

    sidebar in progress

--- src/components/Static/Sidebar.tsx
+++ src/components/Static/Sidebar.tsx
@@ -3,6 +3,8 @@ import styled from 'styled-components'
 import img from "../../assets/KAO_Logo_PNG.png"
 import { FiHome } from "react-icons/fi";
 import { BiStoreAlt } from "react-icons/bi";
+import { IoWalletOutline } from "react-icons/io5";
+import { LuMessagesSquare } from "react-icons/lu";
 import { NavLink } from 'react-router-dom';
 
 const Sidebar = () => {
@@ -35,7 +37,7 @@ const Sidebar = () => {
           height: isActive ? "50px" : "50px",
           width: isActive ? "100%" : "100%",
           backgroundColor: isActive ? "#fff" : "",
-          marginTop: isActive ? "15px" : "15px",
+          marginTop: isActive ? "10px" : "10px",
           paddingLeft: isActive ? "30px" : "30px",
           color: isActive ? "#0030AD" : "#fff",
           alignItems: isActive ? "center" : "center",
@@ -47,11 +49,60 @@ const Sidebar = () => {
           <h3>Stores</h3>
         </Home>
       </NavLink>
+
+      <NavLink to="" style={({ isActive }) => {
+        return {
+          height: isActive ? "50px" : "50px",
+          width: isActive ? "100%" : "100%",
+          backgroundColor: isActive ? "#fff" : "",
+          marginTop: isActive ? "10px" : "10px",
+          paddingLeft: isActive ? "30px" : "30px",
+          color: isActive ? "#0030AD" : "#fff",
+          alignItems: isActive ? "center" : "center",
+          display: isActive ? "flex" : "flex"
+        }
+      }}>
+        <Home>
+          <Icon><IoWalletOutline /></Icon>
+          <h3>Wallet</h3>
+        </Home>
+      </NavLink>
+
+      <NavLink to="" style={({ isActive }) => {
+        return {
+          height: isActive ? "50px" : "50px",
+          width: isActive ? "100%" : "100%",
+          backgroundColor: isActive ? "#fff" : "",
+          marginTop: isActive ? "10px" : "10px",
+          paddingLeft: isActive ? "30px" : "30px",
+          color: isActive ? "#0030AD" : "#fff",
+          alignItems: isActive ? "center" : "center",
+          display: isActive ? "flex" : "flex"
+        }
+      }}>
+        <Home>
+          <Icon><LuMessagesSquare /></Icon>
+          <h3>Messages</h3>
+          <Circle>1</Circle>
+        </Home>
+      </NavLink>
     </Container>
   )
 }
 
 export default Sidebar
+const Circle = styled.div`
+  width: 20px;
+  height: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid red;
+  border-radius: 50px;
+  font-size: 12px;
+  margin-left: 12px;
+  margin-top: 3px;
+`
 const Icon = styled.div`
   font-size: 23px;
 `
