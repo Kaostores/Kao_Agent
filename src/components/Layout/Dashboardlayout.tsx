e88f69e238831d4b9c6561520b0cc624875da098
Okwoli-Godwin <okwolig60@gmail.com>
2024-01-19 12:29:20 +0100

    admin dashboard in progress

--- src/components/Layout/Dashboardlayout.tsx
+++ src/components/Layout/Dashboardlayout.tsx
@@ -1,17 +1,30 @@
 import React from 'react'
 import { Outlet } from "react-router-dom"
 import { Dashboardhead, Sidebar } from '../Static'
+import styled from 'styled-components'
 
 const Dashboardlayout = () => {
   return (
-    <div>
+    <Container>
         <Dashboardhead />
         <Sidebar />
-        <div>
+        <Wrapper>
             <Outlet />
-        </div>
-    </div>
+        </Wrapper>
+    </Container>
   )
 }
 
 export default Dashboardlayout
+const Wrapper = styled.div`
+  width: 100%;
+  display: flex;
+  margin-top: 70px;
+  min-height: 100vh;
+`;
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+`
