bb8211582954bf70f3218a06c599f0966f13fd6d
Okwoli-Godwin <okwolig60@gmail.com>
2024-01-19 17:00:44 +0100

    update

--- src/components/Static/Dashboardhead.tsx
+++ src/components/Static/Dashboardhead.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import styled from 'styled-components'
+import { IoSearch } from "react-icons/io5";
 
 const Dashboardhead = () => {
   return (
     <Container>
       <Wrapper>
         <Inputhold>
-
+          <Icon><IoSearch /></Icon>
+          <input type="text" placeholder='Search'/>
         </Inputhold>
       </Wrapper>
     </Container>
@@ -14,11 +16,27 @@ const Dashboardhead = () => {
 }
 
 export default Dashboardhead
+const Icon = styled.div`
+  color: #0030AD;
+  font-size: 20px;
+`
 const Inputhold = styled.div`
   width: 250px;
   height: 35px;
-  background-color: red;
+  background-color: #fff;
   border-radius: 5px;
+  border: 1px solid #DEE3E9;
+  display: flex;
+  align-items: center;
+  padding-left: 10px;
+  font-size: 14px;
+  overflow: hidden;
+  input{
+    flex: 1;
+    margin-left: 10px;
+    outline: none;
+    height: 100%;
+  }
 `
 const Wrapper = styled.div`
   width: 95%;
