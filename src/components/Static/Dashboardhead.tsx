6bb90241650f4633dcb75a143185630e755c63fa
Okwoli-Godwin <okwolig60@gmail.com>
2024-01-20 08:12:53 +0100

    update

--- src/components/Static/Dashboardhead.tsx
+++ src/components/Static/Dashboardhead.tsx
@@ -1,24 +1,144 @@
 import React from 'react'
 import styled from 'styled-components'
 import { IoSearch } from "react-icons/io5";
+import { LuPrinter, LuFolderOpenDot  } from "react-icons/lu";
+import { IoSettingsOutline } from "react-icons/io5";
+import { IoIosNotificationsOutline } from "react-icons/io";
 
 const Dashboardhead = () => {
   return (
     <Container>
       <Wrapper>
-        <Inputhold>
+        <First>
+          <Inputhold>
           <Icon><IoSearch /></Icon>
           <input type="text" placeholder='Search'/>
         </Inputhold>
+
+        <Calendar>
+          <input type="week" />
+        </Calendar>
+        </First>
+        <Second>
+          <Downloadholder>
+          <Left>
+            <LuPrinter />
+          </Left>
+          <Right>
+            <LuFolderOpenDot />
+          </Right>
+        </Downloadholder>
+        <Setings>
+          <IoSettingsOutline />
+        </Setings>
+        <Notificationholder>
+          <IoIosNotificationsOutline />
+          <Dot></Dot>
+        </Notificationholder>
+        <Profileholder>
+          <Circle></Circle>
+          <h3>Dan Casey</h3>
+        </Profileholder>
+        </Second>
       </Wrapper>
     </Container>
   )
 }
 
 export default Dashboardhead
-const Icon = styled.div`
+const Second = styled.div`
+  display: flex;
+  align-items: center;
+`
+const First = styled.div`
+  display: flex;
+  align-items: center;
+`
+const Circle = styled.div`
+  width: 37px;
+  height: 37px;
+  background-color: #FF7600;
+  border-radius: 100px;
+`
+const Profileholder = styled.div`
+  display: flex;
+  align-items: center;
+  margin-left: 30px;
+  h3{
+    font-size: 16px;
+    margin-left: 10px;
+  }
+`
+const Dot = styled.div`
+  width: 9px;
+  height: 9px;
+  border-radius: 50px;
+  background-color: #0030AD;
+  position: absolute;
+  top: 5px;
+  right: 8px;
+`
+const Notificationholder = styled.div`
+  width: 30px;
+  height: 30px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #C7C7C7;
+  cursor: pointer;
+  font-size: 25px;
+  margin-top: 2px;
+  margin-left: 15px;
+  position: relative;
+`
+const Setings = styled.div`
   color: #0030AD;
   font-size: 20px;
+  cursor: pointer;
+  margin-left: 25px;
+`
+const Right = styled.div`
+  width: 50%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  color: #0030AD;
+  cursor: pointer;
+  align-items: center;
+  background-color: #fff;
+`
+const Left = styled.div`
+  width: 50%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  color: #fff;
+  cursor: pointer;
+  align-items: center;
+  background-color: #0030AD;
+`
+const Downloadholder = styled.div`
+  width: 80px;
+  height: 35px;
+  border: 1px solid #DEE3E9;
+  border-radius: 5px;
+  margin-left: 10px;
+  overflow: hidden;
+  display: flex;
+`
+const Calendar = styled.div`
+  input{
+    width: 200px;
+    height: 35px;
+    border: 1px solid #DEE3E9;
+    border-radius: 5px;
+    outline: none;
+    margin-left: 15px;
+  }
+`
+const Icon = styled.div`
+  color: #0030AD;
+  font-size: 17px;
 `
 const Inputhold = styled.div`
   width: 250px;
@@ -43,6 +163,7 @@ const Wrapper = styled.div`
   height: 100%;
   display: flex;
   align-items: center;
+  justify-content: space-between;
 `
 
 const Container = styled.div`
