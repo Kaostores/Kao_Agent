a5c9de882864ac46862400f38d4b04dfe9fbf682
Okwoli-Godwin <okwolig60@gmail.com>
2024-01-21 15:30:14 +0100

    update

--- src/pages/Dashboard/Cardhold.tsx
+++ src/pages/Dashboard/Cardhold.tsx
@@ -1,18 +1,35 @@
 import React from 'react'
 import styled from 'styled-components'
+import { TiArrowUp } from "react-icons/ti";
 
 const Cardhold = () => {
   return (
     <Container>
         <Card>
             <h3>Customers</h3>
-            <h2>36,254</h2>
+              <h2>36,254</h2>
+              <Percent>
+                  <h4>
+                      <Icon><TiArrowUp /></Icon>
+                      3.27%
+                  </h4>
+              </Percent>
         </Card>
     </Container>
   )
 }
 
 export default Cardhold
+const Icon = styled.div`
+    
+`
+const Percent = styled.div`
+    display: flex;
+    align-items: center;
+    h4{
+        color: #008348;
+    }
+`
 const Card = styled.div`
     width: 200px;
     padding: 10px 13px;
@@ -24,7 +41,7 @@ const Card = styled.div`
         font-size: 19px;
         color: #000;
         font-weight: 500;
-        margin-top: 5px;
+        margin-top: 8px;
     }
     h3{
         font-size: 17px;
