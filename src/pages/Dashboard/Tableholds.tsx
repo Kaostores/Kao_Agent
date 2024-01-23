09ea18ce44fe8b3735f1745c61d4a52c12752ab7
Okwoli-Godwin <okwolig60@gmail.com>
2024-01-23 06:23:59 +0100

    update

--- src/pages/Dashboard/Tableholds.tsx
+++ src/pages/Dashboard/Tableholds.tsx
@@ -1,17 +1,45 @@
 import React from 'react'
 import styled from 'styled-components'
+import { BsThreeDotsVertical } from "react-icons/bs";
 
 const Tableholds = () => {
   return (
     <Container>
-        
+        <First>
+          <Paymentholder>
+            <h3>Payment Method</h3>
+            <Icon><BsThreeDotsVertical /></Icon>
+          </Paymentholder>
+        </First>
     </Container>
   )
 }
 
 export default Tableholds
+const Icon = styled.div`
+  cursor: pointer;
+`
+const Paymentholder = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  h3{
+    font-size: 17px;
+    color: #000;
+  }
+`
+const First = styled.div`
+  width: 48%;
+  display: flex;
+  flex-direction: column;
+  padding: 13px 13px;
+  background-color: #fff;
+  border-radius: 5px;
+`
 const Container = styled.div`
     display: flex;
     height: 150px;
     background-color: red;
+    margin-top: 20px;
 `
