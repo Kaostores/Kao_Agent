d7fc7d2b6745e9dba7adda4ed1531ae85cc09796
Okwoli-Godwin <okwolig60@gmail.com>
2024-01-24 23:41:33 +0100

    update

--- src/pages/Dashboard/Stores.tsx
+++ src/pages/Dashboard/Stores.tsx
@@ -3,6 +3,44 @@ import styled from 'styled-components'
 import { MdOutlineStorefront } from "react-icons/md";
 
 const Stores = () => {
+  const tableItems = [
+        {
+            name: "Solo learn app",
+            date: "Oct 9, 2023",
+            status: "Active",
+            price: "$35.000",
+            plan: "Monthly subscription"
+        },
+        {
+            name: "Window wrapper",
+            date: "Oct 12, 2023",
+            status: "Active",
+            price: "$12.000",
+            plan: "Monthly subscription"
+        },
+        {
+            name: "Unity loroin",
+            date: "Oct 22, 2023",
+            status: "Archived",
+            price: "$20.000",
+            plan: "Annually subscription"
+        },
+        {
+            name: "Background remover",
+            date: "Jan 5, 2023",
+            status: "Active",
+            price: "$5.000",
+            plan: "Monthly subscription"
+        },
+        {
+            name: "Colon tiger",
+            date: "Jan 6, 2023",
+            status: "Active",
+            price: "$9.000",
+            plan: "Annually subscription"
+        },
+    ]
+
   const [show, setShow] = useState(true);
   const [show2, setShow2] = useState(false)
 
@@ -28,69 +66,45 @@ const Stores = () => {
 
         {show ? (
           <Storetableholder>
-            <div className="overflow-x-auto">
-  <table className="min-w-full divide-y-2 divide-gray-200 bg-white text-sm">
-    <thead className="ltr:text-left rtl:text-right">
-      <tr>
-        <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">#</th>
-        <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">Store ID</th>
-        <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">Store Name</th>
-        <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">Address</th>
-        <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">Phone No</th>
-        <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">Email</th>
-        <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">Status</th>
-        <th className="px-4 py-2"></th>
-      </tr>
-    </thead>
-
-    <tbody className="divide-y divide-gray-200">
-      <tr>
-        <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">John Doe</td>
-        <td className="whitespace-nowrap px-4 py-2 text-gray-700">24/05/1995</td>
-        <td className="whitespace-nowrap px-4 py-2 text-gray-700">Web Developer</td>
-        <td className="whitespace-nowrap px-4 py-2 text-gray-700">$120,000</td>
-        <td className="whitespace-nowrap px-4 py-2">
-          <a
-            href="#"
-            className="inline-block rounded bg-indigo-600 px-4 py-2 text-xs font-medium text-white hover:bg-indigo-700"
-          >
-            View
-          </a>
-        </td>
-      </tr>
-
-      <tr>
-        <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">Jane Doe</td>
-        <td className="whitespace-nowrap px-4 py-2 text-gray-700">04/11/1980</td>
-        <td className="whitespace-nowrap px-4 py-2 text-gray-700">Web Designer</td>
-        <td className="whitespace-nowrap px-4 py-2 text-gray-700">$100,000</td>
-        <td className="whitespace-nowrap px-4 py-2">
-          <a
-            href="#"
-            className="inline-block rounded bg-indigo-600 px-4 py-2 text-xs font-medium text-white hover:bg-indigo-700"
-          >
-            View
-          </a>
-        </td>
-      </tr>
-
-      <tr>
-        <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">Gary Barlow</td>
-        <td className="whitespace-nowrap px-4 py-2 text-gray-700">24/05/1995</td>
-        <td className="whitespace-nowrap px-4 py-2 text-gray-700">Singer</td>
-        <td className="whitespace-nowrap px-4 py-2 text-gray-700">$20,000</td>
-        <td className="whitespace-nowrap px-4 py-2">
-          <a
-            href="#"
-            className="inline-block rounded bg-indigo-600 px-4 py-2 text-xs font-medium text-white hover:bg-indigo-700"
-          >
-            View
-          </a>
-        </td>
-      </tr>
-    </tbody>
-  </table>
-</div>
+            <div className="max-w-screen-xl mx-auto px-4 md:px-8">
+            <div className="mt-12 relative h-max overflow-auto">
+                <table className="w-full table-auto text-sm text-left">
+                    <thead className="text-gray-600 font-medium border-b">
+                        <tr>
+                            <th className="py-3 pr-6">#</th>
+                            <th className="py-3 pr-6">Store ID</th>
+                            <th className="py-3 pr-6">Address</th>
+                            <th className="py-3 pr-6">Phone No</th>
+                            <th className="py-3 pr-6">Email</th>
+                            <th className="py-3 pr-6">Status</th>
+                            <th className="py-3 pr-6"></th>
+                        </tr>
+                    </thead>
+                    <tbody className="text-gray-600 divide-y">
+                        {
+                            tableItems.map((item, idx) => (
+                                <tr key={idx}>
+                                    <td className="pr-6 py-4 whitespace-nowrap">{item.name}</td>
+                                    <td className="pr-6 py-4 whitespace-nowrap">{item.date}</td>
+                                    <td className="pr-6 py-4 whitespace-nowrap">
+                                        <span className={`px-3 py-2 rounded-full font-semibold text-xs ${item.status == "Active" ? "text-green-600 bg-green-50" : "text-blue-600 bg-blue-50"}`}>
+                                            {item.status}
+                                        </span>
+                                    </td>
+                                    <td className="pr-6 py-4 whitespace-nowrap">{item.plan}</td>
+                                    <td className="pr-6 py-4 whitespace-nowrap">{item.price}</td>
+                                    <td className="text-right whitespace-nowrap">
+                                        <a href="javascript:void()" className="py-1.5 px-3 text-gray-600 hover:text-gray-500 duration-150 hover:bg-gray-50 border rounded-lg">
+                                            Manage
+                                        </a>
+                                    </td>
+                                </tr>
+                            ))
+                        }
+                    </tbody>
+                </table>
+            </div>
+        </div>
         </Storetableholder>
         ) : null}
 
