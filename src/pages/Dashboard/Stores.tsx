eab0feab2a3ada922d10d0531057a02fe79074d8
Okwoli-Godwin <okwolig60@gmail.com>
2024-01-24 22:51:04 +0100

    update

--- src/pages/Dashboard/Stores.tsx
+++ src/pages/Dashboard/Stores.tsx
@@ -28,7 +28,69 @@ const Stores = () => {
 
         {show ? (
           <Storetableholder>
+            <div className="overflow-x-auto">
+  <table className="min-w-full divide-y-2 divide-gray-200 bg-white text-sm">
+    <thead className="ltr:text-left rtl:text-right">
+      <tr>
+        <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">#</th>
+        <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">Store ID</th>
+        <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">Store Name</th>
+        <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">Address</th>
+        <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">Phone No</th>
+        <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">Email</th>
+        <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">Status</th>
+        <th className="px-4 py-2"></th>
+      </tr>
+    </thead>
 
+    <tbody className="divide-y divide-gray-200">
+      <tr>
+        <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">John Doe</td>
+        <td className="whitespace-nowrap px-4 py-2 text-gray-700">24/05/1995</td>
+        <td className="whitespace-nowrap px-4 py-2 text-gray-700">Web Developer</td>
+        <td className="whitespace-nowrap px-4 py-2 text-gray-700">$120,000</td>
+        <td className="whitespace-nowrap px-4 py-2">
+          <a
+            href="#"
+            className="inline-block rounded bg-indigo-600 px-4 py-2 text-xs font-medium text-white hover:bg-indigo-700"
+          >
+            View
+          </a>
+        </td>
+      </tr>
+
+      <tr>
+        <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">Jane Doe</td>
+        <td className="whitespace-nowrap px-4 py-2 text-gray-700">04/11/1980</td>
+        <td className="whitespace-nowrap px-4 py-2 text-gray-700">Web Designer</td>
+        <td className="whitespace-nowrap px-4 py-2 text-gray-700">$100,000</td>
+        <td className="whitespace-nowrap px-4 py-2">
+          <a
+            href="#"
+            className="inline-block rounded bg-indigo-600 px-4 py-2 text-xs font-medium text-white hover:bg-indigo-700"
+          >
+            View
+          </a>
+        </td>
+      </tr>
+
+      <tr>
+        <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">Gary Barlow</td>
+        <td className="whitespace-nowrap px-4 py-2 text-gray-700">24/05/1995</td>
+        <td className="whitespace-nowrap px-4 py-2 text-gray-700">Singer</td>
+        <td className="whitespace-nowrap px-4 py-2 text-gray-700">$20,000</td>
+        <td className="whitespace-nowrap px-4 py-2">
+          <a
+            href="#"
+            className="inline-block rounded bg-indigo-600 px-4 py-2 text-xs font-medium text-white hover:bg-indigo-700"
+          >
+            View
+          </a>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</div>
         </Storetableholder>
         ) : null}
 
@@ -56,9 +118,9 @@ const Verifiedtableholder = styled.div`
 `
 const Storetableholder = styled.div`
   width: 100%;
-  height: 40px;
   background-color: red;
   margin-top: 20px;
+  padding-top: 10px;
 `
 const Store = styled.div<{bg: string}>`
   width: 50%;
