43b7a721a2ec1613fca2d6b9905843ac2d129ace
Okwoli-Godwin <okwolig60@gmail.com>
2024-01-25 17:06:33 +0100

    update

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import ScrollToTop from './Scrolltotop';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -11,7 +12,8 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ScrollToTop />
+        <App />
     </BrowserRouter>
   </React.StrictMode>
 );
