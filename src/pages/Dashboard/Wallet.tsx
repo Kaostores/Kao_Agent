2ea1e45432291ffe123b30449cd3cf0810417066
Okwoli-Godwin <okwolig60@gmail.com>
2024-01-27 06:46:16 +0100

    update

--- src/pages/Dashboard/Wallet.tsx
+++ src/pages/Dashboard/Wallet.tsx
@@ -1,12 +1,24 @@
 import React from 'react'
 import styled from 'styled-components'
+import { IoWalletOutline } from "react-icons/io5";
 
 const Wallet = () => {
   return (
     <Container>
       <Wrapper>
         <Right>
-
+          <Top>
+            <Balancehold>
+              <Balance>
+                <Icon1><IoWalletOutline /></Icon1>
+                <h3>Balance</h3>
+              </Balance>
+              <Kao>
+                <h3>KAO</h3>
+                <p>NGN</p>
+              </Kao>
+            </Balancehold>
+          </Top>
         </Right>
         <Left>
 
@@ -17,13 +29,54 @@ const Wallet = () => {
 }
 
 export default Wallet
+const Kao = styled.div`
+  display: flex;
+  align-items: center;
+  h3{
+    color: #0030AD;
+    font-size: 15px;
+  }
+  p{
+    color: #C7C7C7;
+    font-size: 13px;
+    font-weight: 600;
+    margin-left: 5px;
+  }
+`
+const Icon1 = styled.div`
+  color: #0030AD;
+  font-size: 20px;
+`
+const Balance = styled.div`
+  display: flex;
+  align-items: center;
+  h3{
+    color: #797979;
+    font-size: 15px;
+    margin-left: 7px;
+  }
+`
+const Balancehold = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`
+const Top = styled.div`
+  width: 100%;
+  padding: 10px 10px;
+  flex-direction: column;
+  background-color: #fff;
+  border-radius: 7px;
+  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+`
 const Left = styled.div`
-  width: 53%;
+  width: 55%;
   display: flex;
   flex-direction: column;
 `
 const Right = styled.div`
-  width: 44%;
+  width: 42%;
   display: flex;
   flex-direction: column;
 `
