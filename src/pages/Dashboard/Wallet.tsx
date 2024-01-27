7a6011d8165f0161f48048cc7dd8f7c1b6d8c700
Okwoli-Godwin <okwolig60@gmail.com>
2024-01-27 06:22:10 +0100

    update

--- src/pages/Dashboard/Wallet.tsx
+++ src/pages/Dashboard/Wallet.tsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import styled from 'styled-components'
 
 const Wallet = () => {
   return (
-    <div>Wallet</div>
+    <Container>
+      <Wrapper>
+
+      </Wrapper>
+    </Container>
   )
 }
 
 export default Wallet
+const Wrapper = styled.div`
+  width: 95%;
+  display: flex;
+  flex-direction: column;
+  background-color: #fff;
+  padding: 13px 13px;
+  height: 40px;
+  background-color: red;
+`
+const Container = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  padding-top: 15px;
+  padding-bottom: 15px;
+`
