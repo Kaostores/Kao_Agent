3e66bbeae882e6336525018e6763a146e5d95671
Okwoli-Godwin <okwolig60@gmail.com>
2024-01-28 14:47:11 +0100

    update

---
+++ src/pages/Dashboard/Withdrawalcard.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Withdrawalcard = () => {
+  return (
+    <Container>
+        
+    </Container>
+  )
+}
+
+export default Withdrawalcard
+
+const Container = styled.div`
+    display: flex;
+    width: 100%;
+    
+`
