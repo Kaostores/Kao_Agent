41a216ff070b87f5505adf088e56467e64f58660
Okwoli-Godwin <okwolig60@gmail.com>
2024-02-07 09:10:42 +0100

    update

--- src/pages/Dashboard/Messages.tsx
+++ src/pages/Dashboard/Messages.tsx
@@ -5,18 +5,29 @@ const Messages = () => {
   return (
     <Container>
       <Wrapper>
-        
+        <Box>
+
+        </Box>
       </Wrapper>
     </Container>
   )
 }
 
 export default Messages
+const Box = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 300px;
+  height: 20px;
+  background-color: #797979;
+`
 
 const Wrapper = styled.div`
   width: 95%;
   display: flex;
-  flex-direction: column;
+  flex-wrap: wrap;
+  height: 20px;
+  background-color: #fff;
 `
 
 const Container = styled.div`
