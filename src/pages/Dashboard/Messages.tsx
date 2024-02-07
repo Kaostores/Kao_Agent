04274f6ac744f3dadf4a55389c80bcaffa78890d
Okwoli-Godwin <okwolig60@gmail.com>
2024-02-07 09:42:20 +0100

    update

--- src/pages/Dashboard/Messages.tsx
+++ src/pages/Dashboard/Messages.tsx
@@ -16,6 +16,61 @@ const Messages = () => {
             <button>View More</button>
           </Buttonhold>
         </Box>
+        <Box>
+          <Id><h4>Store ID: <span>0009021</span></h4></Id>
+          <Id2><h4>Agent ID: <span>34567778</span></h4></Id2>
+          <Id2><h4>Complaint Category: <span>Bribery</span></h4></Id2>
+          <Id2><h4>Store Name: <span>Revolutionary Army</span></h4></Id2>
+          <Id2><h4>Agent Name: <span>Solomon Teach</span></h4></Id2>
+          <Id3><h4>Time|Date: 11:50 | 26/05/2023</h4></Id3>
+          <Buttonhold>
+            <button>View More</button>
+          </Buttonhold>
+        </Box>
+        <Box>
+          <Id><h4>Store ID: <span>0009021</span></h4></Id>
+          <Id2><h4>Agent ID: <span>34567778</span></h4></Id2>
+          <Id2><h4>Complaint Category: <span>Bribery</span></h4></Id2>
+          <Id2><h4>Store Name: <span>Revolutionary Army</span></h4></Id2>
+          <Id2><h4>Agent Name: <span>Solomon Teach</span></h4></Id2>
+          <Id3><h4>Time|Date: 11:50 | 26/05/2023</h4></Id3>
+          <Buttonhold>
+            <button>View More</button>
+          </Buttonhold>
+        </Box>
+        <Box>
+          <Id><h4>Store ID: <span>0009021</span></h4></Id>
+          <Id2><h4>Agent ID: <span>34567778</span></h4></Id2>
+          <Id2><h4>Complaint Category: <span>Bribery</span></h4></Id2>
+          <Id2><h4>Store Name: <span>Revolutionary Army</span></h4></Id2>
+          <Id2><h4>Agent Name: <span>Solomon Teach</span></h4></Id2>
+          <Id3><h4>Time|Date: 11:50 | 26/05/2023</h4></Id3>
+          <Buttonhold>
+            <button>View More</button>
+          </Buttonhold>
+        </Box>
+        <Box>
+          <Id><h4>Store ID: <span>0009021</span></h4></Id>
+          <Id2><h4>Agent ID: <span>34567778</span></h4></Id2>
+          <Id2><h4>Complaint Category: <span>Bribery</span></h4></Id2>
+          <Id2><h4>Store Name: <span>Revolutionary Army</span></h4></Id2>
+          <Id2><h4>Agent Name: <span>Solomon Teach</span></h4></Id2>
+          <Id3><h4>Time|Date: 11:50 | 26/05/2023</h4></Id3>
+          <Buttonhold>
+            <button>View More</button>
+          </Buttonhold>
+        </Box>
+        <Box>
+          <Id><h4>Store ID: <span>0009021</span></h4></Id>
+          <Id2><h4>Agent ID: <span>34567778</span></h4></Id2>
+          <Id2><h4>Complaint Category: <span>Bribery</span></h4></Id2>
+          <Id2><h4>Store Name: <span>Revolutionary Army</span></h4></Id2>
+          <Id2><h4>Agent Name: <span>Solomon Teach</span></h4></Id2>
+          <Id3><h4>Time|Date: 11:50 | 26/05/2023</h4></Id3>
+          <Buttonhold>
+            <button>View More</button>
+          </Buttonhold>
+        </Box>
       </Wrapper>
     </Container>
   )
@@ -45,7 +100,7 @@ const Buttonhold = styled.div`
 const Id3 = styled.div`
   display: flex;
   align-items: center;
-  margin-top: 7px;
+  margin-top: 10px;
   h4{
     color: #797979;
     font-size: 15px;
@@ -58,7 +113,7 @@ const Id3 = styled.div`
 const Id2 = styled.div`
   display: flex;
   align-items: center;
-  margin-top: 7px;
+  margin-top: 10px;
   h4{
     color: #000000;
     font-size: 15px;
@@ -84,11 +139,12 @@ const Id = styled.div`
 const Box = styled.div`
   display: flex;
   flex-direction: column;
-  width: 250px;
+  width: 275px;
   background-color: #0031ad24;
   box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
   border-radius: 5px;
   padding: 10px 10px;
+  margin-bottom: 25px;
 `
 
 const Wrapper = styled.div`
@@ -97,6 +153,7 @@ const Wrapper = styled.div`
   flex-wrap: wrap;
   background-color: #fff;
   padding: 20px 20px;
+  justify-content: space-between;
 `
 
 const Container = styled.div`
