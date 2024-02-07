22fb9426a39628f48fd06b0328e8a08653f48169
Okwoli-Godwin <okwolig60@gmail.com>
2024-02-07 09:02:55 +0100

    update

--- src/pages/Dashboard/Messages.tsx
+++ src/pages/Dashboard/Messages.tsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import styled from "styled-components"
 
 const Messages = () => {
   return (
-    <div>Messages</div>
+    <Container>
+      <Wrapper>
+        
+      </Wrapper>
+    </Container>
   )
 }
 
 export default Messages
+
+const Wrapper = styled.div`
+  width: 95%;
+  display: flex;
+  flex-direction: column;
+`
+
+const Container = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  padding-top: 15px;
+  padding-bottom: 15px;
+`
